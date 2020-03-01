7be0ebb437732419231edb67016e3fe94727e442
iceblade122 <59306482+iceblade122@users.noreply.github.com>
2020-02-29 23:31:42 -0800

    Added Classnames
    Added classes for Afghani, Acapulco Gold, Cookies, Blueberry, Durban Poison, OGKush, Stardawg

--- output/Addons/CannabisPlus/seeds/config.cpp
+++ output/Addons/CannabisPlus/seeds/config.cpp
@@ -61,6 +61,114 @@ class CfgVehicles
 			PlantType="Plant_CannabisKush";
 		};
 	};
+	// NEXT UPDATE
+	class CannabisSeedsAcapulco: SeedBase
+	{
+		scope=2;
+		displayName="Acapulco Gold Cannabis Seeds";
+		descriptionShort="A handful of Acapulco Gold cannabis seeds.";
+		model="\dz\gear\cultivation\cannabis_seeds.p3d";
+		canBeSplit=1;
+		varQuantityInit=20;
+		varQuantityMin=0;
+		varQuantityMax=20;
+		class Horticulture
+		{
+			PlantType="Plant_CannabisAcapulco";
+		};
+	};
+	class CannabisSeedsStardawg: SeedBase
+	{
+		scope=2;
+		displayName="Stardawg Cannabis Seeds";
+		descriptionShort="A handful of Stardawg cannabis seeds.";
+		model="\dz\gear\cultivation\cannabis_seeds.p3d";
+		canBeSplit=1;
+		varQuantityInit=20;
+		varQuantityMin=0;
+		varQuantityMax=20;
+		class Horticulture
+		{
+			PlantType="Plant_CannabisStardawg";
+		};
+	};
+	/* FUTURE
+	class CannabisSeedsAfghani: SeedBase
+	{
+		scope=2;
+		displayName="Afghani Cannabis Seeds";
+		descriptionShort="A handful of Afghani cannabis seeds.";
+		model="\dz\gear\cultivation\cannabis_seeds.p3d";
+		canBeSplit=1;
+		varQuantityInit=20;
+		varQuantityMin=0;
+		varQuantityMax=20;
+		class Horticulture
+		{
+			PlantType="Plant_CannabisAfghani";
+		};
+	};
+	class CannabisSeedsBlueberry: SeedBase
+	{
+		scope=2;
+		displayName="Blueberry Cannabis Seeds";
+		descriptionShort="A handful of Blueberry cannabis seeds.";
+		model="\dz\gear\cultivation\cannabis_seeds.p3d";
+		canBeSplit=1;
+		varQuantityInit=20;
+		varQuantityMin=0;
+		varQuantityMax=20;
+		class Horticulture
+		{
+			PlantType="Plant_CannabisBlueberry";
+		};
+	};
+	class CannabisSeedsCookies: SeedBase
+	{
+		scope=2;
+		displayName="Cookies Cannabis Seeds";
+		descriptionShort="A handful of Cookies cannabis seeds.";
+		model="\dz\gear\cultivation\cannabis_seeds.p3d";
+		canBeSplit=1;
+		varQuantityInit=20;
+		varQuantityMin=0;
+		varQuantityMax=20;
+		class Horticulture
+		{
+			PlantType="Plant_CannabisCookies";
+		};
+	};
+	class CannabisSeedsDurban: SeedBase
+	{
+		scope=2;
+		displayName="Durban Poison Cannabis Seeds";
+		descriptionShort="A handful of Durban Poison cannabis seeds.";
+		model="\dz\gear\cultivation\cannabis_seeds.p3d";
+		canBeSplit=1;
+		varQuantityInit=20;
+		varQuantityMin=0;
+		varQuantityMax=20;
+		class Horticulture
+		{
+			PlantType="Plant_CannabisDurban";
+		};
+	};
+	class CannabisSeedsOGKush: SeedBase
+	{
+		scope=2;
+		displayName="OG Kush Cannabis Seeds";
+		descriptionShort="A handful of OG Kush cannabis seeds.";
+		model="\dz\gear\cultivation\cannabis_seeds.p3d";
+		canBeSplit=1;
+		varQuantityInit=20;
+		varQuantityMin=0;
+		varQuantityMax=20;
+		class Horticulture
+		{
+			PlantType="Plant_CannabisOGKush";
+		};
+	};
+	*/ // endFUTURE
 	class TobaccoSeeds: SeedBase
 	{
 		scope=2;
@@ -400,6 +508,765 @@ class CfgVehicles
 			};
 		};
 	};
+	// NEXT UPDATE
+	class CannabisSeedsPackAcapulco: Inventory_Base
+	{
+		scope=2;
+		displayName="Packed Cannabis Acapulco Gold Seeds";
+		descriptionShort="A package of Acapulco Gold cannabis seeds.";
+		model="\dz\gear\cultivation\zucchini_seeds_pack.p3d";
+		rotationFlags=17;
+		quantityBar=1;
+		itemSize[]={1,1};
+		weight=10;
+		spawnOffset=0;
+		lootCategory="Materials";
+		lootTag[]=
+		{
+			"Farm",
+			"Kitchen",
+			"Forester"
+		};
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"CannabisPlus\seeds\data\cannabis_seeds_pack_acapulco_co.paa"
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=5;
+					healthLevels[]=
+					{
+						
+						{
+							1,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.69999999,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.5,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0.30000001,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+		};
+		class Horticulture
+		{
+			ContainsSeedsType="CannabisSeedsAcapulco";
+		};
+		class UserActions
+		{
+			class EmptyPack
+			{
+				displayNameDefault="Empty Seeds Pack";
+				displayName="A Empty Seeds Pack";
+				position="action";
+				onlyForPlayer=1;
+				radius=2;
+				condition="true";
+				statement="this callMethod ['EmptySeedPack', _person];";
+			};
+		};
+		class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickup
+				{
+					soundSet="seedpack_pickup_SoundSet";
+					id=797;
+				};
+			};
+		};
+	};
+	class CannabisSeedsPackStardawg: Inventory_Base
+	{
+		scope=2;
+		displayName="Packed Cannabis Stardawg Seeds";
+		descriptionShort="A package of Stardawg cannabis seeds.";
+		model="\dz\gear\cultivation\zucchini_seeds_pack.p3d";
+		rotationFlags=17;
+		quantityBar=1;
+		itemSize[]={1,1};
+		weight=10;
+		spawnOffset=0;
+		lootCategory="Materials";
+		lootTag[]=
+		{
+			"Farm",
+			"Kitchen",
+			"Forester"
+		};
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"CannabisPlus\seeds\data\cannabis_seeds_pack_stardawg_co.paa"
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=5;
+					healthLevels[]=
+					{
+						
+						{
+							1,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.69999999,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.5,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0.30000001,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+		};
+		class Horticulture
+		{
+			ContainsSeedsType="CannabisSeedsStardawg";
+		};
+		class UserActions
+		{
+			class EmptyPack
+			{
+				displayNameDefault="Empty Seeds Pack";
+				displayName="A Empty Seeds Pack";
+				position="action";
+				onlyForPlayer=1;
+				radius=2;
+				condition="true";
+				statement="this callMethod ['EmptySeedPack', _person];";
+			};
+		};
+		class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickup
+				{
+					soundSet="seedpack_pickup_SoundSet";
+					id=797;
+				};
+			};
+		};
+	};
+	/* FUTURE
+	class CannabisSeedsPackAfghani: Inventory_Base
+	{
+		scope=2;
+		displayName="Packed Cannabis Afghani Seeds";
+		descriptionShort="A package of Afghani cannabis seeds.";
+		model="\dz\gear\cultivation\zucchini_seeds_pack.p3d";
+		rotationFlags=17;
+		quantityBar=1;
+		itemSize[]={1,1};
+		weight=10;
+		spawnOffset=0;
+		lootCategory="Materials";
+		lootTag[]=
+		{
+			"Farm",
+			"Kitchen",
+			"Forester"
+		};
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"CannabisPlus\seeds\data\cannabis_seeds_pack_afghani_co.paa"
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=5;
+					healthLevels[]=
+					{
+						
+						{
+							1,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.69999999,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.5,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0.30000001,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+		};
+		class Horticulture
+		{
+			ContainsSeedsType="CannabisSeedsAfghani";
+		};
+		class UserActions
+		{
+			class EmptyPack
+			{
+				displayNameDefault="Empty Seeds Pack";
+				displayName="A Empty Seeds Pack";
+				position="action";
+				onlyForPlayer=1;
+				radius=2;
+				condition="true";
+				statement="this callMethod ['EmptySeedPack', _person];";
+			};
+		};
+		class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickup
+				{
+					soundSet="seedpack_pickup_SoundSet";
+					id=797;
+				};
+			};
+		};
+	};
+	class CannabisSeedsPackBlueberry: Inventory_Base
+	{
+		scope=2;
+		displayName="Packed Cannabis Blueberry Seeds";
+		descriptionShort="A package of Blueberry cannabis seeds.";
+		model="\dz\gear\cultivation\zucchini_seeds_pack.p3d";
+		rotationFlags=17;
+		quantityBar=1;
+		itemSize[]={1,1};
+		weight=10;
+		spawnOffset=0;
+		lootCategory="Materials";
+		lootTag[]=
+		{
+			"Farm",
+			"Kitchen",
+			"Forester"
+		};
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"CannabisPlus\seeds\data\cannabis_seeds_pack_blueberry_co.paa"
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=5;
+					healthLevels[]=
+					{
+						
+						{
+							1,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.69999999,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.5,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0.30000001,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+		};
+		class Horticulture
+		{
+			ContainsSeedsType="CannabisSeedsBlueberry";
+		};
+		class UserActions
+		{
+			class EmptyPack
+			{
+				displayNameDefault="Empty Seeds Pack";
+				displayName="A Empty Seeds Pack";
+				position="action";
+				onlyForPlayer=1;
+				radius=2;
+				condition="true";
+				statement="this callMethod ['EmptySeedPack', _person];";
+			};
+		};
+		class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickup
+				{
+					soundSet="seedpack_pickup_SoundSet";
+					id=797;
+				};
+			};
+		};
+	};
+	class CannabisSeedsPackCookies: Inventory_Base
+	{
+		scope=2;
+		displayName="Packed Cannabis Cookies Seeds";
+		descriptionShort="A package of Cookies cannabis seeds.";
+		model="\dz\gear\cultivation\zucchini_seeds_pack.p3d";
+		rotationFlags=17;
+		quantityBar=1;
+		itemSize[]={1,1};
+		weight=10;
+		spawnOffset=0;
+		lootCategory="Materials";
+		lootTag[]=
+		{
+			"Farm",
+			"Kitchen",
+			"Forester"
+		};
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"CannabisPlus\seeds\data\cannabis_seeds_pack_cookies_co.paa"
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=5;
+					healthLevels[]=
+					{
+						
+						{
+							1,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.69999999,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.5,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0.30000001,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+		};
+		class Horticulture
+		{
+			ContainsSeedsType="CannabisSeedsCookies";
+		};
+		class UserActions
+		{
+			class EmptyPack
+			{
+				displayNameDefault="Empty Seeds Pack";
+				displayName="A Empty Seeds Pack";
+				position="action";
+				onlyForPlayer=1;
+				radius=2;
+				condition="true";
+				statement="this callMethod ['EmptySeedPack', _person];";
+			};
+		};
+		class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickup
+				{
+					soundSet="seedpack_pickup_SoundSet";
+					id=797;
+				};
+			};
+		};
+	};
+	class CannabisSeedsPackDurban: Inventory_Base
+	{
+		scope=2;
+		displayName="Packed Cannabis Durban Poison Seeds";
+		descriptionShort="A package of Durban Poison cannabis seeds.";
+		model="\dz\gear\cultivation\zucchini_seeds_pack.p3d";
+		rotationFlags=17;
+		quantityBar=1;
+		itemSize[]={1,1};
+		weight=10;
+		spawnOffset=0;
+		lootCategory="Materials";
+		lootTag[]=
+		{
+			"Farm",
+			"Kitchen",
+			"Forester"
+		};
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"CannabisPlus\seeds\data\cannabis_seeds_pack_durban_co.paa"
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=5;
+					healthLevels[]=
+					{
+						
+						{
+							1,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.69999999,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.5,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0.30000001,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+		};
+		class Horticulture
+		{
+			ContainsSeedsType="CannabisSeedsDurban";
+		};
+		class UserActions
+		{
+			class EmptyPack
+			{
+				displayNameDefault="Empty Seeds Pack";
+				displayName="A Empty Seeds Pack";
+				position="action";
+				onlyForPlayer=1;
+				radius=2;
+				condition="true";
+				statement="this callMethod ['EmptySeedPack', _person];";
+			};
+		};
+		class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickup
+				{
+					soundSet="seedpack_pickup_SoundSet";
+					id=797;
+				};
+			};
+		};
+	};
+	class CannabisSeedsPackOGkush: Inventory_Base
+	{
+		scope=2;
+		displayName="Packed Cannabis OG Kush Seeds";
+		descriptionShort="A package of OG Kush cannabis seeds.";
+		model="\dz\gear\cultivation\zucchini_seeds_pack.p3d";
+		rotationFlags=17;
+		quantityBar=1;
+		itemSize[]={1,1};
+		weight=10;
+		spawnOffset=0;
+		lootCategory="Materials";
+		lootTag[]=
+		{
+			"Farm",
+			"Kitchen",
+			"Forester"
+		};
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"CannabisPlus\seeds\data\cannabis_seeds_pack_ogkush_co.paa"
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=5;
+					healthLevels[]=
+					{
+						
+						{
+							1,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.69999999,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds.rvmat"
+							}
+						},
+						
+						{
+							0.5,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0.30000001,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_damage.rvmat"
+							}
+						},
+						
+						{
+							0,
+							
+							{
+								"DZ\gear\cultivation\data\cannabis_seeds_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+		};
+		class Horticulture
+		{
+			ContainsSeedsType="CannabisSeedsOGKush";
+		};
+		class UserActions
+		{
+			class EmptyPack
+			{
+				displayNameDefault="Empty Seeds Pack";
+				displayName="A Empty Seeds Pack";
+				position="action";
+				onlyForPlayer=1;
+				radius=2;
+				condition="true";
+				statement="this callMethod ['EmptySeedPack', _person];";
+			};
+		};
+		class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickup
+				{
+					soundSet="seedpack_pickup_SoundSet";
+					id=797;
+				};
+			};
+		};
+	};
+	*/ // endFUTURE
 	class TobaccoSeedsPack: Inventory_Base
 	{
 		scope=2;
