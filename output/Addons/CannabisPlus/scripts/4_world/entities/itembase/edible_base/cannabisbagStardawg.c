685a6b859e2733e18bcdb2cbf79a34a329760ce3
iceblade122 <59306482+iceblade122@users.noreply.github.com>
2020-03-18 23:21:21 -0700

    Added Stardawg files.

---
+++ output/Addons/CannabisPlus/scripts/4_world/entities/itembase/edible_base/cannabisbagStardawg.c
@@ -0,0 +1,25 @@
+class CannabisBagStardawg : Edible_Base
+{
+	override bool CanBeCookedOnStick()
+	{
+		return false;
+	}
+
+	override bool CanBeCooked()
+	{
+		return false;
+	}	
+
+	override bool IsFruit()
+	{
+		return true;
+	}
+	
+	override void SetActions()
+	{
+		super.SetActions();
+		
+		AddAction(ActionForceFeed);
+		AddAction(ActionEatSmall);
+	}
+}
