6f59f68a196d8cbc7d45845d1a3588e5ca56806a
iceblade122 <59306482+iceblade122@users.noreply.github.com>
2020-03-18 21:18:41 -0700

    Added Stardawg + fix
    Fixed issue with Bricks being able to be stacked.

--- output/Addons/CannabisPlus/bricks/config.cpp
+++ output/Addons/CannabisPlus/bricks/config.cpp
@@ -22,15 +22,15 @@ class CfgVehicles
 		descriptionShort="One pound of Skunk weed.";
 		model="\dz\gear\containers\FirstAidKit.p3d";
 		rotationFlags=17;
-		canBeSplit=1;
+		//canBeSplit=1;
 		lootCategory="Crafted";
 		weight=450;
 		itemSize[]={4,4};
 		absorbency=0.2;
-		varQuantityInit=1;
-		varQuantityMin=0;
-		varQuantityMax=16;
-		varQuantityDestroyOnMin=1;
+		//varQuantityInit=1;
+		//varQuantityMin=0;
+		//varQuantityMax=16;
+		//varQuantityDestroyOnMin=1;
 		isMeleeWeapon=1;
 		hiddenSelections[]=
 		{
@@ -117,15 +117,15 @@ class CfgVehicles
 		descriptionShort="One pound of Blue God weed.";
 		model="\dz\gear\containers\FirstAidKit.p3d";
 		rotationFlags=17;
-		canBeSplit=1;
+		//canBeSplit=1;
 		lootCategory="Crafted";
 		weight=450;
 		itemSize[]={4,4};
 		absorbency=0.2;
-		varQuantityInit=1;
-		varQuantityMin=0;
-		varQuantityMax=16;
-		varQuantityDestroyOnMin=1;
+		//varQuantityInit=1;
+		//varQuantityMin=0;
+		//varQuantityMax=16;
+		//varQuantityDestroyOnMin=1;
 		isMeleeWeapon=1;
 		hiddenSelections[]=
 		{
@@ -212,15 +212,15 @@ class CfgVehicles
 		descriptionShort="One pound of Purple Kush weed.";
 		model="\dz\gear\containers\FirstAidKit.p3d";
 		rotationFlags=17;
-		canBeSplit=1;
+		//canBeSplit=1;
 		lootCategory="Crafted";
 		weight=450;
 		itemSize[]={4,4};
 		absorbency=0.2;
-		varQuantityInit=1;
-		varQuantityMin=0;
-		varQuantityMax=16;
-		varQuantityDestroyOnMin=1;
+		//varQuantityInit=1;
+		//varQuantityMin=0;
+		//varQuantityMax=16;
+		//varQuantityDestroyOnMin=1;
 		isMeleeWeapon=1;
 		hiddenSelections[]=
 		{
@@ -299,5 +299,102 @@ class CfgVehicles
 				};
 			};
 		};
-	};		
+	};
+	//NEXT UPDATE
+	class CannabisBrickStardawg: Container_Base
+	{	
+		scope=2;
+		displayName="Brick of Stardawg Weed";
+		descriptionShort="One pound of Stardawg weed.";
+		model="\dz\gear\containers\FirstAidKit.p3d";
+		rotationFlags=17;
+		//canBeSplit=1;
+		lootCategory="Crafted";
+		weight=450;
+		itemSize[]={4,4};
+		absorbency=0.2;
+		//varQuantityInit=1;
+		//varQuantityMin=0;
+		//varQuantityMax=16;
+		//varQuantityDestroyOnMin=1;
+		isMeleeWeapon=1;
+		hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"CannabisPlus\bricks\data\cannabis_brick_stardawg_co.paa"
+		};
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=30;
+					healthLevels[]=
+					{
+						
+						{
+							1,
+							
+							{
+								"CannabisPlus\bricks\data\cannabis_brick.rvmat"
+							}
+						},
+						
+						{
+							0.69999999,
+							
+							{
+								"CannabisPlus\bricks\data\cannabis_brick.rvmat"
+							}
+						},
+						
+						{
+							0.5,
+							
+							{
+								"CannabisPlus\bricks\data\cannabis_brick_damage.rvmat"
+							}
+						},
+						
+						{
+							0.30000001,
+							
+							{
+								"CannabisPlus\bricks\data\cannabis_brick_damage.rvmat"
+							}
+						},
+						
+						{
+							0,
+							
+							{
+								"CannabisPlus\bricks\data\cannabis_brick_destruct.rvmat"
+							}
+						}
+					};
+				};
+			};
+		};
+		class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickUpItem_Light
+				{
+					soundSet="pickUpCourierBag_Light_SoundSet";
+					id=796;
+				};
+				class pickUpItem
+				{
+					soundSet="pickUpCourierBag_SoundSet";
+					id=797;
+				};
+			};
+		};
+	};
+	//end NEXT UPDATE
 };
