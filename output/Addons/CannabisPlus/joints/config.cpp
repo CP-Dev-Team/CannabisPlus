a53684f742b77416b279054dc5773f6a6070fec9
iceblade122 <59306482+iceblade122@users.noreply.github.com>
2020-03-18 22:00:50 -0700

    Added stardawg joint

--- output/Addons/CannabisPlus/joints/config.cpp
+++ output/Addons/CannabisPlus/joints/config.cpp
@@ -65,7 +65,6 @@ class CfgVehicles
 		*/
 	};
 
-
 	class CP_JointBlue: Edible_Base
 	{
 		scope=2;
@@ -172,6 +171,53 @@ class CfgVehicles
 			plugType=7;
 			attachmentAction=1;
 		};
+		*/
+	};
+
+	class CP_JointStardawg: Edible_Base
+	{
+		scope=2;
+		displayName="Joint";
+		descriptionShort="A joint made out of finest Stardawg weed";
+		model="\CannabisPlus\joints\joint.p3d";
+		repairableWithKits[]={5,8};
+		repairCosts[]={30,25};
+		inventorySlot="Ingredient";
+		lootCategory="Crafted";
+		SingleUseActions[]={509,510};
+		InteractActions[]={1002,1003};
+		rotationFlags=16;
+		weight=30;
+		itemSize[]={1,2};
+		absorbency=0;
+		varQuantityInit=500;
+		varQuantityMin=0;
+		varQuantityMax=500;
+		//varLiquidTypeInit=0;
+		//liquidContainerType="1 + 2 + 4 + 8 + 16 + 32 + 64 + 128 + 256 + 512 + 1024 + 2048 + 4096 + 8192 + 16384 + 32768 + 65536  - (1 + 2 + 4 + 8 + 16 + 32 + 64 + 128 + 256)";
+		heatIsolation=0.13;
+		quantityBar=1;
+		//visibilityModifier=0.69999999;
+		class Nutrition {
+			
+			totalVolume=1;
+			fullnessIndex=2.5;
+			energy=1;
+			water=-10;
+			nutritionalIndex=1;
+			toxicity=0;
+		};
+		/*
+		class EnergyManager {
+            switchOnAtSpawn=0;
+            isPassiveDevice=1;
+            energyStorageMax=60;
+            energyUsagePerSecond=1;
+            energyAtSpawn=60;
+            powerSocketsCount=1;
+            compatiblePlugTypes[]={7};
+            convertEnergyToQuantity=1;
+        };
 		*/
 	};
 };
