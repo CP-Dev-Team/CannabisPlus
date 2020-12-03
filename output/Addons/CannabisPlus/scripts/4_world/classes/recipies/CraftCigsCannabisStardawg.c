90dda72667260c9048b215eaf34f568c6a21df10
iceblade122 <59306482+iceblade122@users.noreply.github.com>
2020-12-03 10:22:39 -0800

    logging the crafting of joints

--- output/Addons/CannabisPlus/scripts/4_world/classes/recipies/CraftCigsCannabisStardawg.c
+++ output/Addons/CannabisPlus/scripts/4_world/classes/recipies/CraftCigsCannabisStardawg.c
@@ -66,5 +66,6 @@ class CP_CraftCigsCannabisStardawg extends RecipeBase
 	override void Do(ItemBase ingredients[], PlayerBase player,array<ItemBase> results, float specialty_weight)//gets called upon recipe's completion
 	{
 		Debug.Log("Recipe Do method called","recipes");
+		CPApi().Increment("CraftJoint.Stardawg");
 	}
 };
