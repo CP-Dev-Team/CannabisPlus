a5e6ebd5e3162f8774f76bb9144be50e05d54fd9
iceblade122 <59306482+iceblade122@users.noreply.github.com>
2020-12-02 17:23:53 -0800

    Update CraftCigsCannabisSkunk.c

--- output/Addons/CannabisPlus/scripts/4_world/classes/recipies/CraftCigsCannabisSkunk.c
+++ output/Addons/CannabisPlus/scripts/4_world/classes/recipies/CraftCigsCannabisSkunk.c
@@ -65,6 +65,6 @@ class CP_CraftCigsCannabisSkunk extends RecipeBase
 	override void Do(ItemBase ingredients[], PlayerBase player,array<ItemBase> results, float specialty_weight)//gets called upon recipe's completion
 	{
 //		Debug.Log("Recipe Do method called","recipes");
-		CPApi().Increment("CraftCigsCannabisSkunk");
+		CPApi().Increment("CraftJoint.Skunk");
 	}
 };
