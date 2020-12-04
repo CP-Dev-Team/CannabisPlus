081f34a30cb1c5af612412d64f420c7add56d99b
iceblade122 <59306482+iceblade122@users.noreply.github.com>
2020-12-03 17:30:14 -0800

    TakeItem logging

---
+++ output/Addons/CannabisPlus/scripts/4_world/PickupItemsAction.c
@@ -0,0 +1,19 @@
+modded class ActionTakeItem: ActionInteractBase
+{
+   
+    override void OnExecuteServer( ActionData action_data )
+    {
+        super.OnExecuteServer(action_data);
+        EntityAI obj = EntityAI.Cast(action_data.m_Target.GetObject());
+        if (obj){
+            if ( obj.GetType() == "CP_RollingPapers" ) 
+            {
+                CPApi().Increment("PickedUp.RollingPapers");
+            }
+			if ( obj.GetType() == "CP_Relief_Balm" ) 
+            {
+                CPApi().Increment("PickedUp.CBDReliefBalm");
+            }
+        }
+    }
+}
