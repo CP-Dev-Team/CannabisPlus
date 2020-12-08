9e800e3f924829fb89027e1f0e9b2961bef5db1d
iceblade122 <59306482+iceblade122@users.noreply.github.com>
2020-12-07 17:20:21 -0800

    Alternate logging method

--- output/Addons/CannabisPlus/scripts/4_world/entities/itembase/cbdpainbase.c
+++ output/Addons/CannabisPlus/scripts/4_world/entities/itembase/cbdpainbase.c
@@ -14,4 +14,44 @@ class CP_Relief_Balm extends Inventory_Base
 		}
 		player.GetModifiersManager().ActivateModifier( eModifiers.MDF_PAINKILLERS );
 	}
+
+	override void OnItemLocationChanged(EntityAI old_owner, EntityAI new_owner)
+    {
+        super.OnItemLocationChanged(old_owner, new_owner);
+
+        if (GetGame().IsServer()) {
+
+            DayZPlayer player_old = NULL;
+            DayZPlayer player_new = NULL;
+            
+            bool CBDCreamPickedUp = false;
+            bool CBDCreamDropped = false;
+            
+            if (old_owner) {
+                if (Class.CastTo(player_old , old_owner) || Class.CastTo(player_old , old_owner.GetHierarchyRootPlayer() ) ) {
+                    if (player_old.GetIdentity()){
+                    CBDCreamDropped = true;
+                    }
+                }
+            }
+            
+            if (new_owner) {
+                if ( Class.CastTo(player_new , new_owner) || Class.CastTo(player_new , new_owner.GetHierarchyRootPlayer() ) ) {
+                    if (player_new.GetIdentity()){
+                    CBDCreamPickedUp = true;
+                    }
+                }
+            }
+            if (CBDCreamPickedUp && CBDCreamDropped && player_new != player_old ) { //Also making sure the player isn't just moving it around in the inventory 
+            //          Print("CBDCream stolen off a deady player's body");
+            CPApi().Increment("PickedUp.RollingPapers");
+            } else if (CBDCreamPickedUp && player_new != player_old ){
+            //          Print("Player has picked up CBDCream");
+            CPApi().Increment("PickedUp.RollingPapers");
+            } else if (CBDCreamDropped && player_new != player_old) {
+            Print("Player has dropped CBDCream");
+            //          CPApi().Increment("PickedUp.RollingPapers");
+        }
+      }
+    }
 };
--- output/Addons/CannabisPlus/scripts/4_world/entities/itembase/gear/rollingpaperpack.c
+++ output/Addons/CannabisPlus/scripts/4_world/entities/itembase/gear/rollingpaperpack.c
@@ -1,39 +1,42 @@
 class CP_RollingPapers: Inventory_Base 
 {
-/*    override void OnItemLocationChanged(EntityAI old_owner, EntityAI new_owner)
+    override void OnItemLocationChanged(EntityAI old_owner, EntityAI new_owner)
     {
         super.OnItemLocationChanged(old_owner, new_owner);
-        
-        DayZPlayer player_old = NULL;
-        DayZPlayer player_new = NULL;
-        
-        bool PapesPickedUp = false;
-        bool PapesDropped = false;
-        
-        if (old_owner) {
-            if (Class.CastTo(player_old , old_owner) || Class.CastTo(player_old , old_owner.GetHierarchyRootPlayer()) {
-                if (player_old.GetIdentity()){
-                   PapesDropped = true;
+
+        if (GetGame().IsServer()) {
+
+            DayZPlayer player_old = NULL;
+            DayZPlayer player_new = NULL;
+            
+            bool PapesPickedUp = false;
+            bool PapesDropped = false;
+            
+            if (old_owner) {
+                if (Class.CastTo(player_old , old_owner) || Class.CastTo(player_old , old_owner.GetHierarchyRootPlayer() ) ) {
+                    if (player_old.GetIdentity()){
+                    PapesDropped = true;
+                    }
                 }
             }
-        }
-        
-        if (new_owner) {
-            if ( Class.CastTo(player_new , new_owner) || Class.CastTo(player_new , new_owner.GetHierarchyRootPlayer() ) {
-                if (player_new.GetIdentity()){
-                   PapesPickedUp = true;
+            
+            if (new_owner) {
+                if ( Class.CastTo(player_new , new_owner) || Class.CastTo(player_new , new_owner.GetHierarchyRootPlayer() ) ) {
+                    if (player_new.GetIdentity()){
+                    PapesPickedUp = true;
+                    }
                 }
             }
+            if (PapesPickedUp && PapesDropped && player_new != player_old ) { //Also making sure the player isn't just moving it around in the inventory 
+            //          Print("Papes stolen off a deady player's body");
+            CPApi().Increment("PickedUp.RollingPapers");
+            } else if (PapesPickedUp && player_new != player_old ){
+            //          Print("Player has picked up Papes");
+            CPApi().Increment("PickedUp.RollingPapers");
+            } else if (PapesDropped && player_new != player_old) {
+            Print("Player has dropped Papes");
+            //          CPApi().Increment("PickedUp.RollingPapers");
         }
-       if (PapesPickedUp && PapesDropped && player_new != player_old ) { //Also making sure the player isn't just moving it around in the inventory 
-//          Print("Papes stolen off a deady player's body");
-          CPApi().Increment("PickedUp.RollingPapers");
-       } else if (PapesPickedUp){
-//          Print("Player has picked up Papes");
-          CPApi().Increment("PickedUp.RollingPapers");
-       } else if (PapesDropped) {
-          Print("Player has dropped Papes");
-//          CPApi().Increment("PickedUp.RollingPapers");
-       }
-    } */
+      }
+    }
 };
