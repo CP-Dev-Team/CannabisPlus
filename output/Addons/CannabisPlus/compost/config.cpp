67b6e12d89e9b7f11a9189adf5ebdcdf9bf071da
Michael Kulikowski <mkulikow@gmail.com>
2021-02-21 11:37:46 -0500

    working compost bin, makes 1 lime per 18 plant material

---
+++ output/Addons/CannabisPlus/compost/config.cpp
@@ -0,0 +1,65 @@
+class CfgPatches
+{
+    class CP_Composter
+    {
+        units[] =
+        {
+            ""
+        };
+        weapons[] = {};
+        requiredVersion = 0.1;
+        requiredAddons[] = 
+        {
+            "DZ_Data"
+        };
+    };
+};
+
+class CfgVehicles
+{
+    class Inventory_Base;
+    class Container_Base;
+    class CP_Composter : Container_Base
+    {
+            scope = 2;
+		displayName = "Compost Bin";
+		descriptionShort = "A bin for converting plant material into compost for fertilizer.";
+		model="\CannabisPlus\compost\compost_bin2.p3d";
+		weight=20000;
+		itemSize[]={9,9};
+		varQuantityDestroyOnMin=1;
+		itemBehaviour=0;
+		canBeSplit=1;
+		varQuantityInit=1;
+		varQuantityMin=0;
+		varQuantityMax=50;
+            varStackMax=1;
+            //inventorySlot[]={};
+            physLayer="item_heavy";
+		itemsCargoSize[] = {10,10};
+            hiddenSelections[]=
+		{
+			"zbytek"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"CannabisPlus\compost\data\LP_Crate_diffuse.tga.paa"
+		};
+            class AnimEvents
+		{
+			class SoundWeapon
+			{
+				class pickup
+				{
+					soundSet="woodenlog_pickup_SoundSet";
+					id=797;
+				};
+				class drop
+				{
+					soundset="woodenlog_drop_SoundSet";
+					id=898;
+				};
+			};
+		};
+    };
+};
---
+++ output/Addons/CannabisPlus/scripts/4_world/entities/itembase/compost.c
@@ -0,0 +1,138 @@
+class CP_Composter extends Container_Base
+{
+
+	bool m_IsLocked = false;
+	EntityAI target;
+	int CompostTick = 10000;   //tick checker every 10s to look for dry material
+	int ProcessTime = 30000;  //2 mins to make compost
+	int NumItems = 0;
+	
+      void CP_Composter()
+	{
+		Init();
+	}
+	
+	void ~CP_Composter()
+	{
+		
+	}
+	
+	void Init()
+	{
+		GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(CheckCargoTick, CompostTick, true);
+	}
+	
+	void CheckCargoTick()
+	{
+		CargoBase cargo = GetInventory().GetCargo();
+		int NumPlantMaterial = 0;
+		int NumRawPlant = 0;
+		string ItemName;	
+		ItemBase item;
+		
+		if (cargo != NULL && !m_IsLocked )
+		{
+			NumItems = 0;
+			for (int i = 0; i < cargo.GetItemCount(); ++i)
+			{
+				if ( cargo.GetItemCount() != 0)
+				{
+					item = ItemBase.Cast(cargo.GetItem(i));
+					ItemName  = item.GetType();
+					if (ItemName.IndexOf("PlantMaterial") >= 0)
+					{
+						NumItems += 1;
+						NumPlantMaterial += 1;
+					} else if (ItemName.IndexOf("CP_Dried") >= 0)
+					{
+						NumItems += 1;
+						NumRawPlant += 1;
+					}
+				}		
+			}
+		}
+		if (NumItems >= 18)
+		//process compost if at least 9 material in bin
+		{
+			if (!m_IsLocked)
+			{  
+				ProcessCompost();
+				Print("[CP] processing compost with " + NumItems + " material");
+			}	
+		}
+	
+	}
+	
+	void ProcessCompost()
+	{
+		m_IsLocked = true;
+		GetInventory().LockInventory(HIDE_INV_FROM_SCRIPT);
+		GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(ProcessOutput, ProcessTime, false);	
+	}
+	
+	void ProcessOutput()
+	{
+		vector output_position = this.GetPosition() + "-1 0 -1";
+		string ItemName;
+		ItemBase item;
+		int j, NumCargo;
+		
+		if ( GetGame() && GetGame().IsServer() )
+		{
+			CargoBase cargo = GetInventory().GetCargo();
+			NumCargo = cargo.GetItemCount();
+			if (NumCargo >= 17)
+			{
+				for (j = 0; j < 18; j++)
+				{
+					item = ItemBase.Cast(cargo.GetItem(j));
+					if (item)
+					{
+						Print("[CP] deleting " + item + " from compost bin");
+						item.Delete();	
+					}	
+				}
+				ItemBase output = ItemBase.Cast(GetGame().CreateObject("GardenLime", output_position, false ));
+			}	
+		}
+		syncronize();
+		GetGame().GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(UnlockInventory, 1000, false);
+	}
+	
+	void UnlockInventory()
+	{
+		m_IsLocked = false;
+		GetInventory().UnlockInventory(HIDE_INV_FROM_SCRIPT);	
+	}
+	
+	override bool CanPutIntoHands(EntityAI parent)
+    	{
+        	if ( m_IsLocked)
+        	{
+            	return false;
+        	}
+        	return super.CanPutIntoHands(parent);
+    	}
+	
+	void syncronize()
+	{
+		if ( GetGame() && GetGame().IsServer() )
+		{
+			SetSynchDirty();
+		}
+	}
+	
+	override string GetPlaceSoundset()
+	{
+		return "woodenlog_drop_SoundSet";
+	}
+    
+	override void SetActions()
+	{
+		super.SetActions();
+		
+		AddAction(ActionTogglePlaceObject);
+		AddAction(ActionPlaceObject);
+	}
+
+}      
