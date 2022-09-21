1e70a6b1b4e2926649fa67547d72d41b4bc677bd
iceblade122 <59306482+iceblade122@users.noreply.github.com>
2022-09-20 20:54:14 -0700

    Fixing rolling joints

--- output/Addons/CannabisPlus/scripts/4_world/classes/recipies/Smart_Recipe_Testing.c
+++ output/Addons/CannabisPlus/scripts/4_world/classes/recipies/Smart_Recipe_Testing.c
@@ -204,8 +204,8 @@ class CP_CraftCigsCannabisBlackFrost extends RecipeBase
 
 		m_IngredientAddHealth[0] = 0;// 0 = do nothing
 		m_IngredientSetHealth[0] = -1; // -1 = do nothing
-		m_IngredientAddQuantity[0] = 0;// 0 = do nothing
-		m_IngredientDestroy[0] = true;//true = destroy, false = do nothing
+		m_IngredientAddQuantity[0] = -1;// 0 = do nothing
+		m_IngredientDestroy[0] = false;//true = destroy, false = do nothing
 		m_IngredientUseSoftSkills[0] = false;// set 'true' to allow modification of the values by softskills on this ingredient
 		
 		//ingredient 2
