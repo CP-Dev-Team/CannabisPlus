f15c350243380ca8d75d859be25d4ba592dc7143
iceblade122 <59306482+iceblade122@users.noreply.github.com>
2025-03-26 14:31:09 -0700

    Whole stack of logs deleted if crafting from a pile.

--- output/Addons/CannabisPlus/scripts/4_world/classes/recipes/CarveWoodenPost.c
+++ output/Addons/CannabisPlus/scripts/4_world/classes/recipes/CarveWoodenPost.c
@@ -28,8 +28,8 @@ class CP_CarveWoodenPost extends RecipeBase
 
 		m_IngredientAddHealth[0] = 0;// 0 = do nothing
 		m_IngredientSetHealth[0] = -1; // -1 = do nothing
-		m_IngredientAddQuantity[0] = 0;// 0 = do nothing
-		m_IngredientDestroy[0] = true;//true = destroy, false = do nothing
+		m_IngredientAddQuantity[0] = -1;// 0 = do nothing
+		m_IngredientDestroy[0] = false;//true = destroy, false = do nothing
 		m_IngredientUseSoftSkills[0] = false;// set 'true' to allow modification of the values by softskills on this ingredient
 		
 		//ingredient 2
