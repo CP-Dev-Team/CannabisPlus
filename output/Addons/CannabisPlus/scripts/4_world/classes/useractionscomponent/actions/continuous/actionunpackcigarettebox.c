8f962f8c831c0e9db3f9e62a6f35d3ec8bdb3a0b
iceblade122 <59306482+iceblade122@users.noreply.github.com>
2025-11-18 19:29:01 -0800

    Changing joint packs to dynamic system

--- output/Addons/CannabisPlus/scripts/4_world/classes/useractionscomponent/actions/continuous/actionunpackcigarettebox.c
+++ output/Addons/CannabisPlus/scripts/4_world/classes/useractionscomponent/actions/continuous/actionunpackcigarettebox.c
@@ -34,53 +34,42 @@ class ActionUnpackCigaretteBox: ActionContinuousBase {
     }
 	
 	override void OnFinishProgressServer( ActionData action_data )
-	{		
+	{
 		super.OnFinishProgressServer(action_data);
-		
-		if ( action_data.m_MainItem && action_data.m_MainItem.GetHierarchyRootPlayer() == action_data.m_Player ) {						
-			
-			for(int i = 0; i<action_data.m_MainItem.GetQuantity(); i++){
-				
-				switch(action_data.m_MainItem.GetType()) {
-					
-					case "CP_CigarettePack_CannabisBlue":
-						resultItem = ItemBase.Cast( GetGame().CreateObject("CP_JointBlue" , action_data.m_Player.GetPosition(), false) );						
-						break;					
-					case "CP_CigarettePack_CannabisKush":
-						resultItem = ItemBase.Cast( GetGame().CreateObject("CP_JointKush" , action_data.m_Player.GetPosition(), false) );
-						break;					
-					case "CP_CigarettePack_CannabisSkunk":
-						resultItem = ItemBase.Cast( GetGame().CreateObject("CP_JointSkunk" , action_data.m_Player.GetPosition(), false) );
-						break;
-					case "CP_CigarettePack_CannabisStardawg":
-						resultItem = ItemBase.Cast( GetGame().CreateObject("CP_JointStardawg" , action_data.m_Player.GetPosition(), false) );
-						break;	
-					case "CP_CigarettePack_CannabisFuture":
-						resultItem = ItemBase.Cast( GetGame().CreateObject("CP_JointFuture" , action_data.m_Player.GetPosition(), false) );
-						break;		
-					case "CP_CigarettePack_CannabisS1":
-						resultItem = ItemBase.Cast( GetGame().CreateObject("CP_JointS1" , action_data.m_Player.GetPosition(), false) );
-						break;		
-					case "CP_CigarettePack_CannabisNomad":
-						resultItem = ItemBase.Cast( GetGame().CreateObject("CP_JointNomad" , action_data.m_Player.GetPosition(), false) );
-						break;					
-					case "CP_CigarettePack_CannabisBlackFrost":
-						resultItem = ItemBase.Cast( GetGame().CreateObject("CP_JointBlackFrost" , action_data.m_Player.GetPosition(), false) );
-						break;		
-					case "CigarettePack_Merkur":
-						resultItem = ItemBase.Cast( GetGame().CreateObject("CP_Cigarette" , action_data.m_Player.GetPosition(), false) );
-						break;					
-					case "CigarettePack_Chernamorka":
-						resultItem = ItemBase.Cast( GetGame().CreateObject("CP_Cigarette" , action_data.m_Player.GetPosition(), false) );
-						break;					
-					case "CigarettePack_Partyzanka":
-						resultItem = ItemBase.Cast( GetGame().CreateObject("CP_Cigarette" , action_data.m_Player.GetPosition(), false) );
-						break;
+
+		if ( action_data.m_MainItem && action_data.m_MainItem.GetHierarchyRootPlayer() == action_data.m_Player ) {
+
+			string packType = action_data.m_MainItem.GetType();
+			CPDebugPrint("Unpacking cigarette pack type: " + packType);
+
+			string jointType = "CP_CigarettePack_CannabisSkunk"; // default for vanilla packs
+
+			if (packType.Contains("CP_CigarettePack_Cannabis"))
+			{
+				string strainName = packType.Substring(25, packType.Length() - 25); // Extract strain name after "CP_CigarettePack_Cannabis"
+				CPDebugPrint("Extracted strain name: " + strainName);
+
+				if (g_CannabisStrainConfigs.Contains(strainName))
+				{
+					jointType = "CP_Joint" + strainName;
+					CPDebugPrint("Using joint type: " + jointType + " for strain: " + strainName);
+				}
+				else
+				{
+					CPDebugPrint("Warning: Strain config for '" + strainName + "' not found. Using default joint type: " + jointType);
 				}
 			}
-			
+			else
+			{
+				CPDebugPrint("Vanilla cigarette pack, using joint type: " + jointType);
+			}
+
+			for(int i = 0; i<action_data.m_MainItem.GetQuantity(); i++){
+				resultItem = ItemBase.Cast( GetGame().CreateObject(jointType , action_data.m_Player.GetPosition(), false) );
+			}
+
 			GetGame().ObjectDelete(action_data.m_MainItem);
 			action_data.m_Player.GetHumanInventory().CreateInHands("CP_CigarettePack_Empty");
 		}
-	}	
+	}
 };
